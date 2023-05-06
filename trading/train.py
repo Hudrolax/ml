7857cd2f8a613f205df4da8d4e2c8b25728bf5a8
Hudrolax <hudro795@gmail.com>
2023-05-06 19:53:21 -0300

    bf

--- trading/train.py
+++ trading/train.py
@@ -5,14 +5,14 @@ logging.basicConfig(format='%(asctime)s: %(message)s',
                     datefmt='%d/%m/%y %H:%M:%S', level=logging.INFO)
 
 pipeline_kwargs = dict(
-    symbols=[''],
+    symbols=['DOGEUSDT'],
     tfs=['15m'],
     env_classes=['TradingEnv1BoxAction'],
     testers=['BBTester'],
     # features_extractors=['Flatten', 'CustomCNN1dReverse', 'CustomCNN2d', 'CustomCNN1d'],
     features_extractors=['CustomCNN2d'],
     value_nets=['mlp_256_64'],
-    b_size=3000,
+    b_size=1000,
     batch_size=128,
     total_timesteps=int(6e6),
     indicators=dict(
